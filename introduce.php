bea2bb84c1d11ddb1a977f1dce2536184ddb4484
HsuWilliam <38956291+HsuWilliam@users.noreply.github.com>
2019-02-09 01:09:33 +0800

    Add files via upload

---
+++ introduce.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>ETicket註冊說明</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+  
+</head>
+<body>
+<div class = "header">
+    <h2>ETicket註冊說明</h2>
+    <div class="input-group">
+    <button type="submit" name="META" class="btn" onclick=window.open("https://metamask.io/")>Metamask</button>
+    </div>
+    <br>
+    <p>Step1.安裝Metamask套件<p>
+    <img src="./img/1.png" width="100%">
+    <br>
+    <p>Step2.點選加到chrome<p>
+    <img src="./img/2.png" width="100%">
+    <br>
+    <p>Step3.點選continue<p>
+    <img src="./img/3.png" width="100%">
+    <br>
+    <p>Step4.創立自己錢包的密碼<p>
+    <img src="./img/4.png" width="100%">
+    <br>
+    <p>Step5.點選Accept<p>
+    <img src="./img/5.png" width="100%">
+    <br>
+    <p>Step6.記住上方單字之順序<p>
+    <img src="./img/6.png" width="100%">
+    <br>
+    <p>Step7.依照前一步驟之單字順序做認證<p>
+    <img src="./img/7.png" width="100%">
+    <br>
+    <p>Step8.電子錢包創立完成，接下來進行平台會員註冊<p>
+    
+</div>
+<br>
+<center><button type="submit" style="width:120px;height:40px;font-size:20px;" class="btn" onclick="location.href='register.php'" >Go!</button><center>
+<br>
+</body>
+</html>
